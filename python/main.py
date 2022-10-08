3d4cfbf81c3129ca380b886bdb7b14600cf3515c
Anand J <anandj@cs.toronto.edu>
2022-10-08 15:25:47 -0400

    Add ItemCache

--- python/main.py
+++ python/main.py
@@ -23,11 +23,10 @@ class Sessions:
         tmp['time'] = tmp.apply(lambda row: row['items'][0].time, axis = 1)
         tmp['count'] = tmp.apply(lambda row: len(row['items']), axis = 1)
         tmp['duration'] = tmp.apply(lambda row: (row['items'][-1].time - row['items'][0].time), axis = 1)
-        tmp['target'] = tmp.apply(lambda row: Item(row['target_item_id'], row['target_time']), axis = 1)
         tmp['last_item'] = tmp.apply(lambda row: row['items'][-1], axis = 1)
-        tmp['items'] = tmp.apply(lambda row: self._append(row['items'], row['target']), axis = 1)
+        tmp['items'] = tmp.apply(lambda row: self._append(row['items'], Item(row['target_item_id'], row['target_time'])), axis = 1)
 
-        tmp = tmp.drop(['item_ids', 'times', 'target_item_id', 'target_time'], axis=1)
+        tmp = tmp.drop(['item_ids', 'times'], axis=1)
 
         tmp = tmp.set_index(['session_id'])
 
@@ -52,14 +51,22 @@ class Sessions:
 
 class ItemCache:
     def __init__(self, start, end, sess, items):
-        print(sess)
-        print(items)
-        self.session_count = items[['session_id', 'item_id']] \
-            .groupby('item_id', group_keys=False) \
-            .agg({"session_id": pd.Series.nunique, })
+        item_sess = items \
+                .merge(sess, on='session_id') \
+                [['item_id', 'session_id', 'time', 'target_item_id', 'target_time', 'count', 'duration']]
 
-train_sessions = pd.read_csv("/home/anandj/data/code/Layer/dressipi_recsys2022/train_sessions.csv", parse_dates=True, nrows=100)
-train_purchases = pd.read_csv("/home/anandj/data/code/Layer/dressipi_recsys2022/train_purchases.csv", parse_dates=True, nrows=1000)
+        self.sess_features = item_sess.groupby('item_id', group_keys=True) \
+                .agg({'session_id': pd.Series.nunique, 'count': pd.Series.sum, 'duration': pd.Series.sum})
+
+
+        self.buy_features = item_sess[item_sess['item_id'] == item_sess['target_item_id']].groupby('item_id', group_keys=True) \
+                .agg({'session_id': pd.Series.nunique, 'count': pd.Series.sum, 'duration': pd.Series.sum})
+
+        print('sess_features', self.sess_features)
+        print('buy_features', self.buy_features)
+
+train_sessions = pd.read_csv("/home/anandj/data/code/Layer/dressipi_recsys2022/train_sessions.csv", parse_dates=True, nrows=10000)
+train_purchases = pd.read_csv("/home/anandj/data/code/Layer/dressipi_recsys2022/train_purchases.csv", parse_dates=True, nrows=10000)
 train_sessions['date'] = pd.to_datetime(train_sessions['date'])
 train_purchases['date'] = pd.to_datetime(train_purchases['date'])
 print(train_sessions)
@@ -72,7 +79,6 @@ start_month = pd.to_datetime("2020-01-01 00:00:00")
 month = relativedelta(months=1)
 end_month = pd.to_datetime("2021-06-01 00:00:00")
 cur_month = start_month
-print(start_month)
 
 while cur_month < end_month:
     start = cur_month
